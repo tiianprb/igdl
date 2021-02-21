89b14860030285c03984531d2e472ae2424fb099
yasin javar <yasinjjjv@gmail.com>
2021-02-21 07:24:39 +0000

    add start message and dl regex

--- bot.py
+++ bot.py
@@ -1,9 +1,13 @@
-from pyrogram import Client
+from pyrogram import Client, filters
 
 api_id = 1691381
 api_hash = "a4b862a161f7a320d5524730bbf51bf2"
-token = ""
+token = "1622516881:AAFSmoSck8T49NRGz6GqsknTJfYFujYtagg"
 bot = Client("sefl-v-02", api_id, api_hash,
              workers=4, plugins=dict(root="plugins"), bot_token=token)
+
+
+bot.set_parse_mode('md')
+
 
 bot.run()
