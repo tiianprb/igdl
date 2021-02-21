e2bdc923103588f8a8337d03c6b5270dfafbe1dd
yasin javar <yasinjjjv@gmail.com>
2021-02-21 07:09:13 +0000

    add plugins files

--- bot.py
+++ bot.py
@@ -1,1 +1,9 @@
-print("hello world")
+from pyrogram import Client
+
+api_id = 1691381
+api_hash = "a4b862a161f7a320d5524730bbf51bf2"
+token = ""
+bot = Client("sefl-v-02", api_id, api_hash,
+             workers=4, plugins=dict(root="plugins"), bot_token=token)
+
+bot.run()
