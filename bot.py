95c9058de431d13670452d533d484fd7726fc367
yasin javar <yasinjjjv@gmail.com>
2021-02-21 07:31:12 +0000

    add start message and dl regex

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 api_id = 1691381
 api_hash = "a4b862a161f7a320d5524730bbf51bf2"
 token = "1622516881:AAFSmoSck8T49NRGz6GqsknTJfYFujYtagg"
-bot = Client("sefl-v-02", api_id, api_hash,
+bot = Client("igdl", api_id, api_hash,
              workers=4, plugins=dict(root="plugins"), bot_token=token)
 
 
