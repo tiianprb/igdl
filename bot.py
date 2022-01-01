0cee3490083b66e1e65fb9f11486a8ae48f60de3
tiianprb <96933129+tiianprb@users.noreply.github.com>
2022-01-01 08:01:46 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 from pyrogram import Client, filters
 
-api_id = 1691381
-api_hash = "a4b862a161f7a320d5524730bbf51bf2"
-token = "1622516881:AAFSmoSck8T49NRGz6GqsknTJfYFujYtagg"
+api_id = 14448837
+api_hash = "06b79ae95202e27931b152dba1903459"
+token = "5099807719:AAEdd2nm4c0qM5ksZAQjo3XaqYNq3Hmfj50"
 bot = Client("igdl", api_id, api_hash,
              workers=4, plugins=dict(root="plugins"), bot_token=token)
 
